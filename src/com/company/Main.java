1ae3e99996d09782a3cbf3b074896fee6c78ef6c
stefanibl <133694715+stefanibl@users.noreply.github.com>
2023-05-18 10:16:14 +0200

    Add files via upload
    My first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import javax.xml.namespace.QName;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GIT");
+        sayHello("Stefani");
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello "+name);
+
+    }
+
+}
